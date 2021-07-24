168391344b9853ab2d2b79de814d61e093f0fce2
Serdar Salci <serdarsalci@gmail.com>
2021-07-23 22:47:44 -0400

    part 294 --PlaceList & Place Provider Setup

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+      ),
+    );
+  }
+}
