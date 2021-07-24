1618fd03f801e1fc63e4246c81d3dcece5328547
Serdar Salci <serdarsalci@gmail.com>
2021-07-24 14:24:17 -0400

    part 296 -- Using Image Picker & Device Camera

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -10,6 +11,12 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage = File('');
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 800);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +49,7 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: TextButton.icon(
-              onPressed: () {},
+              onPressed: _takePicture,
               icon: Icon(Icons.camera),
               label: Text('Take Picture')),
         ),
